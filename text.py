17b4c1723672bae37001ea3cf260e3d650692a30
akshaykpandey18 <akshaykpandey18@yahoo.com>
2026-02-08 16:40:41 +0530

    Update text.py

--- text.py
+++ text.py
@@ -32,7 +32,7 @@ def download_latest(user, repo, fallback_tag, output="latest_release.zip"):
 
 payload = json.loads(sys.argv[1])
 print(type(payload))
-data1 = json.loads(payload["data1"])
+data1 = payload["data1"]
 print(data1)
 print(type(data1))
 targets = [tuple(x) for x in data1]
